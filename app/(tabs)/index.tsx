e562dc4bfeca49e815483e30e81c6a4139316e31
Ryan Ayers <rcayers@icloud.com>
2024-12-21 14:07:11 -0600

    Added button and logic for edit mode

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,34 @@
-import React from 'react'
-import { Text, View, StyleSheet, Dimensions} from 'react-native';
-import { FlashList } from "@shopify/flash-list";
+import { useState } from 'react';
+import { FlatList, View, StyleSheet, Button,} from 'react-native';
 
 import Input from '@/components/Input';
 
 export default function Index() {
 
   const songs = [
-    {title:"Bit Better"},
-    {title:"Back to Back"},
-    {title:"YNBA"},
-    {title:"Lightburst"},
-    {title:"Getting Time"},
-    {title:"New Years"}
+    { title: "Bit Better" },
+    { title: "Back to Back" },
+    { title: "YNBA" },
+    { title: "Lightburst" },
+    { title: "Getting Time" },
+    { title: "New Years" }
   ];
+
+  const [editMode, setEditMode] = useState(false);
 
   return (
     <View style={styles.container}>
-
-      <FlashList
-        data={songs}
-        renderItem={({item}) => <Input defaultText={item.title}/>}
-        estimatedItemSize={10}
+      <Button
+        onPress={() => setEditMode(!editMode)}
+        title={editMode ? "Save" : "Edit"}
+        color={editMode ? "#8FBC8F" : "#F08080"}
+        accessibilityLabel={editMode ? "Save this setlist" : "Edit this setlist"}
       />
 
+      <FlatList 
+        data={songs}
+        renderItem={({ item }) => <Input defaultText={item.title} editMode={editMode} />}
+      />
     </View>
   );
 }
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,32 +1,49 @@
 import { useState } from 'react';
-import { Text, View, StyleSheet, TextInput } from 'react-native';
+import { View, StyleSheet, TextInput, Text } from 'react-native';
 
 
 type Props = {
   defaultText: string;
+  editMode: boolean;
 }
 
-export default function Input({ defaultText }: Props) {
+export default function Input({ defaultText, editMode }: Props) {
 
   const [text, setText] = useState(defaultText);
 
   return (
     <View>
-      <TextInput
-        style={styles.input}
-        onChangeText={newText => setText(newText)}
-        value={text}
-      />
+      {editMode ?
+        <TextInput
+          style={styles.input}
+          onChangeText={newText => setText(newText)}
+          value={text}
+        /> :
+        <Text style={styles.notEditing}>{text}</Text>
+      }
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   input: {
+    borderRadius: 12,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    color: '#25292e',
+    borderColor: '#fff',
+    backgroundColor: '#fff'
+  },
+  notEditing: {
+    borderRadius: 12,
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
     color: '#fff',
-    borderColor: '#fff'
+    borderColor: '#fff',
+    alignSelf: 'flex-start'
   }
 });
